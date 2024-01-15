0fe518499722db8b8fbc34f1f235f33b8d587162
Krushna Anil Ipar <krushnaipar5551@gmail.com>
2024-01-15 11:30:43 +0530

    Create Compare_Two_Numbers.cpp

---
+++ Lecture 003/Compare_Two_Numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    int a, b;
+
+    cout << "Enter the value of a:" << endl;
+    cin >> a;
+
+    cout << "Enter the value of b :" << endl;
+    cin >> b;
+
+    if (a > b)
+    {
+        cout << "a is greater than b" << endl;
+    }
+
+    if (b > a)
+    {
+        cout << "b is greater than a" << endl;
+    }
+
+}
